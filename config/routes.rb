7dccf4a8281eaea37309c1e48835e66721a7bf57
RyanWD <ryan.saunders86@icloud.com>
2020-05-17 21:15:49 +0100

    reddit clone - 30% progress

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :accounts
+  get "u/:username" => "public#profile", as: :profile
+
+  resources :communities do
+    resources :posts
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "public#index"
+end
